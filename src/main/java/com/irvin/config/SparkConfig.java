49b175a8430974efd11ad22995029806ec43e558
IrvinX <171427088@qq.com>
2017-08-18 15:18:06 +0800

    thread

--- src/main/java/com/irvin/config/SparkConfig.java
+++ src/main/java/com/irvin/config/SparkConfig.java
@@ -1,60 +1,60 @@
-package com.irvin.config;
-
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
-
-import java.io.Serializable;
-
-/**
- * @author irvin
- * @date Create in 上午2:52 2017/8/5
- * @description
- */
-@Configuration
-@PropertySource("classpath:application.properties")
-public class SparkConfig implements Serializable {
-
-    /**
-     *
-     */
-    private static final long serialVersionUID = -5017266850264030516L;
-
-    @Autowired
-    private Environment env;
-
-    @Value("${app.name}")
-    private String appName;
-
-    @Value("${spark.home}")
-    private String sparkHome;
-
-    @Value("${master.uri}")
-    private String masterUri;
-
-    @Bean
-    public SparkConf sparkConf() {
-        SparkConf sparkConf = new SparkConf().setAppName(appName)
-                // .setSparkHome(sparkHome)
-                .setMaster(masterUri);
-
-        return sparkConf;
-    }
-
-    @Bean
-    public JavaSparkContext javaSparkContext() {
-        return new JavaSparkContext(sparkConf());
-    }
-
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-        return new PropertySourcesPlaceholderConfigurer();
-    }
-}
+//package com.irvin.config;
+//
+//import org.apache.spark.SparkConf;
+//import org.apache.spark.api.java.JavaSparkContext;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+//import org.springframework.core.env.Environment;
+//
+//import java.io.Serializable;
+//
+///**
+// * @author irvin
+// * @date Create in 上午2:52 2017/8/5
+// * @description
+// */
+//@Configuration
+//@PropertySource("classpath:application.properties")
+//public class SparkConfig implements Serializable {
+//
+//    /**
+//     *
+//     */
+//    private static final long serialVersionUID = -5017266850264030516L;
+//
+//    @Autowired
+//    private Environment env;
+//
+//    @Value("${app.name}")
+//    private String appName;
+//
+//    @Value("${spark.home}")
+//    private String sparkHome;
+//
+//    @Value("${master.uri}")
+//    private String masterUri;
+//
+//    @Bean
+//    public SparkConf sparkConf() {
+//        SparkConf sparkConf = new SparkConf().setAppName(appName)
+//                // .setSparkHome(sparkHome)
+//                .setMaster(masterUri);
+//
+//        return sparkConf;
+//    }
+//
+//    @Bean
+//    public JavaSparkContext javaSparkContext() {
+//        return new JavaSparkContext(sparkConf());
+//    }
+//
+//
+//    @Bean
+//    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+//        return new PropertySourcesPlaceholderConfigurer();
+//    }
+//}
--- src/main/java/com/irvin/service/sax/SaxXMLHandler.java
+++ src/main/java/com/irvin/service/sax/SaxXMLHandler.java
@@ -26,156 +26,148 @@ import java.util.concurrent.Executors;
 @Component
 public class SaxXMLHandler extends DefaultHandler {
 
-    private static Logger logger = LoggerFactory.getLogger(SaxXMLHandler.class);
+	private static final int MAX_SIZE = 256;
+	private static Logger logger = LoggerFactory.getLogger(SaxXMLHandler.class);
+	@Autowired
+	IndexService indexService;
+	/*
+		存放遍历集合
+	 */
+	private List<Artical> list;
+	/*
+		Article 内部属性
+	 */
+	private List<String> meshTerms = new ArrayList<>();
+	private List<String> textblocks = new ArrayList<>();
+	/*
+		构建 Artical 对象
+	 */
+	private Artical artical;
+	/*
+		用来存放每次遍历后的元素名称(节点名称)
+	 */
+	private String tagName;
+	/*
+		计数器
+	 */
+	private Long count = 0L;
+	/*
+		Lucene IndexWriter
+	 */
+	private IndexWriter indexWriter;
+	/*
+		线程池
+	 */
+	private ExecutorService singleThreadPool;
 
-    @Autowired
-    IndexService indexService;
+	public SaxXMLHandler() {
+		this.singleThreadPool = Executors.newSingleThreadExecutor();
+		try {
+			this.indexWriter = IndexUtil.getIndexWriter("index");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public SaxXMLHandler() {
-        this.fixedThreadPool = Executors.newFixedThreadPool(3);
-        try {
-            this.indexWriter = IndexUtil.getIndexWriter("");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	public Long getCount() {
+		return count;
+	}
 
-    public Long getCount() {
-        return count;
-    }
+	public IndexWriter getIndexWriter() {
+		return indexWriter;
+	}
 
-    private static final int MAX_SIZE = 1024;
-    //存放遍历集合
-    private List<Artical> list;
-    //Article 内部属性
-    private List<String> meshTerms = new ArrayList<>();
-    private List<String> textblocks = new ArrayList<>();
-    //构建 Artical 对象
-    private Artical artical;
-    //用来存放每次遍历后的元素名称(节点名称)
-    private String tagName;
-    private Long count = 0L;
-    private IndexWriter indexWriter;
-    private ExecutorService fixedThreadPool;
+	public List<Artical> getList() {
+		return list;
+	}
 
-    public IndexWriter getIndexWriter() {
-        return indexWriter;
-    }
-
-    public List<Artical> getList() {
-        return list;
-    }
-
-    //只调用一次  初始化list集合
-    @Override
-    public void startDocument() throws SAXException {
-        logger.debug("Start to analyse xml ...");
-        list = new ArrayList<>();
-    }
-
-
-    //调用多次    开始解析
-    @Override
-    public void startElement(String uri, String localName, String qName,
-                             Attributes attributes) throws SAXException {
-        if (qName.equals("DOC")) {// 一个文档的开始
-            artical = new Artical();
-        }
-        this.tagName = qName;
-    }
-
-
-    //调用多次
-    @Override
-    public void endElement(String uri, String localName, String qName)
-            throws SAXException {
-        if (qName.equals("DOC")) {
-            this.list.add(this.artical);
-            count++;
-        }
-
-        if (MAX_SIZE == list.size()) {
-            List<Artical> threadList = new ArrayList<>();
-            threadList.addAll(list);
-            list.clear();
-
-            fixedThreadPool.execute(() -> {
-                try {
-                    // 调 lucene 创建索引
-                    indexService.indexFiles(threadList, indexWriter);
-                    indexWriter.commit();
-                    indexWriter.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-//            IndexThread indexThread = new IndexThread(list);
-//            indexThread.start();
-        }
-        this.tagName = null;
-    }
+	//只调用一次  初始化list集合
+	@Override
+	public void startDocument() throws SAXException {
+		logger.debug("Start to analyse xml ...");
+		list = new ArrayList<>();
+	}
 
 
-    //只调用一次
-    @Override
-    public void endDocument() throws SAXException {
-    }
+	//调用多次    开始解析
+	@Override
+	public void startElement(String uri, String localName, String qName,
+							 Attributes attributes) throws SAXException {
+		if (qName.equals("DOC")) {// 一个文档的开始
+			artical = new Artical();
+		}
+		this.tagName = qName;
+	}
 
-    //调用多次
-    @Override
-    public void characters(char[] ch, int start, int length) throws SAXException {
-        if (this.tagName != null) {
-            String data = new String(ch, start, length);
-            if (this.tagName.equals("DOCNO")) {
-                this.artical.setDocNo(data);
-            } else if (this.tagName.equals("brief_title")) {
-                this.artical.setBriefTitle(data);
-            } else if (this.tagName.equals("condition")) {
-                this.artical.setCondition(data);
-            } else if (this.tagName.equals("intervention_type")) {
-                this.artical.setInterventionType(data);
-            } else if (this.tagName.equals("intervention_name")) {
-                this.artical.setInterventionName(data);
-            } else if (this.tagName.equals("description")) {
-                this.artical.setDescription(data);
-            } else if (this.tagName.equals("arm_group_label")) {
-                this.artical.setArmGroupLabel(data);
-            } else if (this.tagName.equals("gender")) {
-                this.artical.setGender(data);
-            } else if (this.tagName.equals("minimum_age")) {
-                this.artical.setMinimumAge(data);
-            } else if (this.tagName.equals("maximum_age")) {
-                this.artical.setMaximumAge(data);
-            } else if (this.tagName.equals("healthy_volunteers")) {
-                this.artical.setHealthyVolunteers(data);
-            } else if (this.tagName.equals("mesh_term")) {
-                meshTerms.add(data);
-                this.artical.setMeshTerms(meshTerms);
-            } else if (this.tagName.equals("textblock")) {
-                textblocks.add(data);
-                this.artical.setTextblocks(textblocks);
-            }
-        }
-    }
 
-//    public class IndexThread extends Thread {
-//
-//        private List<Artical> articals;
-//
-//        public IndexThread(List<Artical> articals) {
-//            this.articals = articals;
-//        }
-//
-//        @Override
-//        public void run() {
-//            // 调 lucene 创建索引
-//            indexService.indexFiles(articals, indexWriter);
-//            try {
-//                indexWriter.commit();
-//                indexWriter.close();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
+	//调用多次
+	@Override
+	public void endElement(String uri, String localName, String qName)
+			throws SAXException {
+		if (qName.equals("DOC")) {
+			this.list.add(this.artical);
+			count++;
+		}
+
+		if (MAX_SIZE == list.size()) {
+			List<Artical> threadList = new ArrayList<>();
+			threadList.addAll(list);
+			list.clear();
+
+			singleThreadPool.execute(() -> {
+				try {
+					// 调 lucene 创建索引
+					indexService.indexFiles(threadList, indexWriter);
+					indexWriter.commit();
+					indexWriter.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+		}
+		this.tagName = null;
+	}
+
+
+	//只调用一次
+	@Override
+	public void endDocument() throws SAXException {
+	}
+
+	//调用多次
+	@Override
+	public void characters(char[] ch, int start, int length) throws SAXException {
+		if (this.tagName != null) {
+			String data = new String(ch, start, length);
+			if (this.tagName.equals("DOCNO")) {
+				this.artical.setDocNo(data);
+			} else if (this.tagName.equals("brief_title")) {
+				this.artical.setBriefTitle(data);
+			} else if (this.tagName.equals("condition")) {
+				this.artical.setCondition(data);
+			} else if (this.tagName.equals("intervention_type")) {
+				this.artical.setInterventionType(data);
+			} else if (this.tagName.equals("intervention_name")) {
+				this.artical.setInterventionName(data);
+			} else if (this.tagName.equals("description")) {
+				this.artical.setDescription(data);
+			} else if (this.tagName.equals("arm_group_label")) {
+				this.artical.setArmGroupLabel(data);
+			} else if (this.tagName.equals("gender")) {
+				this.artical.setGender(data);
+			} else if (this.tagName.equals("minimum_age")) {
+				this.artical.setMinimumAge(data);
+			} else if (this.tagName.equals("maximum_age")) {
+				this.artical.setMaximumAge(data);
+			} else if (this.tagName.equals("healthy_volunteers")) {
+				this.artical.setHealthyVolunteers(data);
+			} else if (this.tagName.equals("mesh_term")) {
+				meshTerms.add(data);
+				this.artical.setMeshTerms(meshTerms);
+			} else if (this.tagName.equals("textblock")) {
+				textblocks.add(data);
+				this.artical.setTextblocks(textblocks);
+			}
+		}
+	}
 }
